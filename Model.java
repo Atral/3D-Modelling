212e5c4fb4041f02a57459664cd28d3ccfda92b7
Jack Maskell <jack_maskell@hotmail.co.uk>
2020-11-19 19:53:01 +0000

    pbr shader attempt

--- Model.java
+++ Model.java
@@ -20,7 +20,7 @@ public class Model {
   private Camera camera;
   private Light light;
   
-  public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh, int[] textureId1, int[] textureId2) {
+  public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh, int[] textureId1, int[] textureId2, int[] textureId3, int[] textureId4, int[] textureId5, int[] textureId6, int[] textureId7) {
     this.mesh = mesh;
     this.material = material;
     this.modelMatrix = modelMatrix;
@@ -29,14 +29,26 @@ public class Model {
     this.light = light;
     this.textureId1 = textureId1;
     this.textureId2 = textureId2;
+    this.textureId3 = textureId3;
+    this.textureId4 = textureId4;
+    this.textureId5 = textureId5;
+    this.textureId6 = textureId6;
+    this.textureId7 = textureId7;
   }
   
+  public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh, int[] textureId3, int[]textureId4, int[] textureId5, int[] textureId6, int[] textureId7){
+    this(gl, camera, light, shader, material, modelMatrix, mesh, null, null, textureId3, textureId4, textureId5, textureId6, textureId7);
+  }
+
+  public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh, int[] textureId1, int[]textureId2){
+    this(gl, camera, light, shader, material, modelMatrix, mesh, textureId1, textureId2, null, null, null, null, null);
+  }
   public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh, int[] textureId1) {
-    this(gl, camera, light, shader, material, modelMatrix, mesh, textureId1, null);
+    this(gl, camera, light, shader, material, modelMatrix, mesh, textureId1, null, null, null, null, null, null);
   }
   
   public Model(GL3 gl, Camera camera, Light light, Shader shader, Material material, Mat4 modelMatrix, Mesh mesh) {
-    this(gl, camera, light, shader, material, modelMatrix, mesh, null, null);
+    this(gl, camera, light, shader, material, modelMatrix, mesh, null, null, null, null, null, null, null);
   }
   
   public void setModelMatrix(Mat4 m) {
@@ -67,7 +79,12 @@ public class Model {
     shader.setVec3(gl, "material.ambient", material.getAmbient());
     shader.setVec3(gl, "material.diffuse", material.getDiffuse());
     shader.setVec3(gl, "material.specular", material.getSpecular());
-    shader.setFloat(gl, "material.shininess", material.getShininess());  
+    shader.setFloat(gl, "material.shininess", material.getShininess());
+
+    shader.setFloat(gl, "material.albedo", material.getAlbedo());
+    shader.setFloat(gl, "material.normal", material.getNormal());
+    shader.setFloat(gl, "material.metallic", material.getMetallic());
+    shader.setFloat(gl, "materal.roughness", material.getRoughness());
 
     if (textureId1!=null) {
       shader.setInt(gl, "first_texture", 0);  // be careful to match these with GL_TEXTURE0 and GL_TEXTURE1
@@ -79,6 +96,32 @@ public class Model {
       gl.glActiveTexture(GL.GL_TEXTURE1);
       gl.glBindTexture(GL.GL_TEXTURE_2D, textureId2[0]);
     }
+    if (textureId3!=null) {
+      shader.setInt(gl, "third_texture", 2);
+      gl.glActiveTexture(GL.GL_TEXTURE2);
+      gl.glBindTexture(GL.GL_TEXTURE_2D, textureId3[0]);
+    }
+    if (textureId4!=null) {
+      shader.setInt(gl, "fourth_texture", 3);
+      gl.glActiveTexture(GL.GL_TEXTURE3);
+      gl.glBindTexture(GL.GL_TEXTURE_2D, textureId4[0]);
+    }
+    if (textureId5!=null) {
+      shader.setInt(gl, "fifth_texture", 4);
+      gl.glActiveTexture(GL.GL_TEXTURE4);
+      gl.glBindTexture(GL.GL_TEXTURE_2D, textureId5[0]);
+    }
+    if (textureId6!=null) {
+      shader.setInt(gl, "sixth_texture", 5);
+      gl.glActiveTexture(GL.GL_TEXTURE5);
+      gl.glBindTexture(GL.GL_TEXTURE_2D, textureId6[0]);
+    }
+    if (textureId7!=null) {
+      shader.setInt(gl, "seventh_texture", 6);
+      gl.glActiveTexture(GL.GL_TEXTURE6);
+      gl.glBindTexture(GL.GL_TEXTURE_2D, textureId7[0]);
+    }
+
     mesh.render(gl);
   } 
   
@@ -90,6 +133,11 @@ public class Model {
     mesh.dispose(gl);
     if (textureId1!=null) gl.glDeleteBuffers(1, textureId1, 0);
     if (textureId2!=null) gl.glDeleteBuffers(1, textureId2, 0);
+    if (textureId2!=null) gl.glDeleteBuffers(1, textureId3, 0);
+    if (textureId2!=null) gl.glDeleteBuffers(1, textureId4, 0);
+    if (textureId2!=null) gl.glDeleteBuffers(1, textureId5, 0);
+    if (textureId2!=null) gl.glDeleteBuffers(1, textureId6, 0);
+    if (textureId2!=null) gl.glDeleteBuffers(1, textureId7, 0);
   }
   
 }
